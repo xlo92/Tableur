0d91838b3c04d54dadb163c37fd27ee6d94f906e
xlo92 <losco.xavier@gmail.com>
2017-12-17 11:39:28 +0100

    Modification Tableur

--- src/tableur/Tableur.java
+++ src/tableur/Tableur.java
@@ -7,39 +7,50 @@ public class Tableur {
 	private CellContainer cells;
 	
 	public Tableur() {
-            this.cells = new CellContainer();
+        this.cells = new CellContainer();
 	}
 	
 	public void enregistrer(String filename) {
-            Fichier f = new Fichier(filename);
-            
-            f.ecrireFichier(cells);
+        Fichier f = new Fichier(filename);
+        if(f.IsOnDisk()) f.makeBackUp();
+        f.ecrireFichier(cells);
 	}
 	
 	public void ouvrir(String filename) {
-            File fic = new File(filename);
-            
-            Fichier f = new Fichier(filename);
-            
-            this.cells = f.lireFichier(fic);
+		char c;
+		String s;
+		Fichier f = new Fichier(filename);
+		if(f.IsOnDisk()) {
+	        File fic = new File(filename);
+	        this.cells = f.lireFichier(fic);
+		}else {
+			this.cells = new CellContainer();
+			for(int i = 0; i < 20; i++) {
+				c = (char)('A'+i);
+				for(int j = 1; j < 21; j++) {
+					s=""+c+j;
+					cells.add(new Cellule(s));
+				}
+			}
+		}
 	}
 	
 	public void supprimerDonnee(String idCell) {
-            Cellule c = this.cells.getCellule(idCell);
-            if(c!=null) {
-            	c.clear();
-            	cells.majDependantes(idCell);
-            }
+        Cellule c = this.cells.getCellule(idCell);
+        if(c!=null) {
+        	c.clear();
+        	cells.majDependantes(idCell);
+        }
 	}
 
 	public void modifierDonnee(String idCell, String data) {
-            Cellule c = this.cells.getCellule(idCell);
-            if(c!=null) {
-            	c.affecterContenu(data);
-            	Interpreter i = new Interpreter(cells);
-            	c.affecterValeur(i.evaluer(data));
-            	cells.majDependantes(idCell);
-            }
+        Cellule c = this.cells.getCellule(idCell);
+        if(c!=null) {
+        	c.affecterContenu(data);
+        	Interpreter i = new Interpreter(cells);
+        	c.affecterValeur(i.evaluer(data));
+        	cells.majDependantes(idCell);
+        }
 	}
 	
 	public void propagerDonnee(String idBase, String idCible) {
